0a753a386aec0e9025173f152a6a8b89e03da22e
snandi <snandi@altir.com>
2023-07-19 20:28:37 +0530

    testing jenkins pipeline

--- dev.tfvars
+++ dev.tfvars
@@ -3,4 +3,5 @@ owner = "hyperian"
 amid = "ami-069d73f3235b535bd"
 instance_size = "t2.micro"
 acl_type = "private"
+bucketname = "dev-s3-testing-tf-jenkins"
 
--- variables.tf
+++ variables.tf
@@ -1,16 +1,11 @@
 variable "owner" {
   default = "altir"
 }
-
-variable "environment" {
-}
-
-variable "amid" {
-}
-variable "acl_type"{
-}
-variable "instance_size" {
-}
+variable "environment" {}
+variable "amid" {}
+variable "acl_type"{}
+variable "instance_size" {}
+variable "bucketname" {}
 locals {
   tags = {
     owner       = var.owner
